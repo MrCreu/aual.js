41a467c55ecb6415f66c6139cdd450d9f04cab08
MrCreu <142320120+MrCreu@users.noreply.github.com>
2023-08-15 11:14:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function novo( )
+{
+    varnome=document.formizinho.nome.value
+    varrua=document.formizinho.rua.value
+    
+{
+
+document.write('A Correios Av.  Brg. Faria lima')
+document.write("Nome:"+varnome+"<br>")
+document.write("Rua:"+varrua+"<br>")
+
+document.write("Atenciosamente,<br><br>")
+document.write("Julio Batista,<br>")
+document.write("Presidente da ABC/SUZ Training Corporation<br>")
+document.write("Treinamento com qualidade com custo reduzido")
+}
+}
